0a5d859ee904d04a5e780cf34d9171f018a79d41
Celina Yang <celinayang18@hotmail.com>
2025-11-25 23:21:13 -0500

    initial commit with postgresql user credentials

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.sql.*;
+
+public class Main {
+    // postgresql user credentials
+    static String url = "jdbc:postgresql://localhost:5432/FinalProject";
+    static String user = "postgres";
+    static String password = "d56988d6";
+
+    static Connection connection;
+    static Statement statement;
+}
