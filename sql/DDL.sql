724187ca5f87d244de44b1223f835847f492cc41
neelaksh <Neelaksh.gupta05@gmail.com>
2025-12-01 12:11:32 -0500

    updated DDL file

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -2,6 +2,7 @@ CREATE TABLE Member (
     member_id      SERIAL PRIMARY KEY,
     first_name           VARCHAR(100) NOT NULL,
     last_name           VARCHAR(100) NOT NULL,
+    password            VARCHAR(100) NOT NULL,
     email          VARCHAR(100) UNIQUE NOT NULL,
     phone          VARCHAR(20) UNIQUE,
     gender         VARCHAR(20)
